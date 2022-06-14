2c147fa3f1e1481157da06164352af10aea12340
Dastan255 <dastan_askaraliev@mail.ru>
2022-06-14 18:46:16 +0600

    14.06.22

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,18 @@
+import React,{ useState } from 'react';
+import Post from '../Post/Post';
+
+const Main = () => {
+
+    return (
+        <main>
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+       </main>
+    );
+};
+
+export default Main;
---
+++ src/components/Post/Post.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Post = () => {
+    return (
+        <div className='post'>
+            <h5 className='title'>The first title</h5>
+        </div>
+    );
+};
+
+export default Post;
